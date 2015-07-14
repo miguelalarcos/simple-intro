a667d1d6d3a4c87d5f0350571ebb853128dcc237
miguelalarcos <miguel.alarcos@gmail.com>
2015-07-14 13:22:30 +0200

    style

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'miguelalarcos:simple-intro',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'A simple intro (tutorial) for Meteor apps.',
   // URL to the Git repository containing the source code for this package.
